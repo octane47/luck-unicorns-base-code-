86a359b6ff124c700f697e438de03968057e1e4e
Nikole Hunter <>
2021-05-20 22:36:32 +0000

    I have put all codes together in one repl whilst also finishing up 18 statement  generator

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from main import *
+import random
 
 # Funtions go here...
 def yes_no(question):
@@ -21,8 +22,18 @@ def yes_no(question):
 def instructions():
     print("**** HOW TO PLAY ****")
     print()
-    print("give me your money")
+    print("choose a starting amount (minimum $1, maximum $10).")
     print()
+    print("then press <enter> to play. you will get either a horse, a zebra, a donkey or a unicorn")
+    print()
+    print("it costs a $1 per round. depending on your prize you might win some money back. here the payout amounts..." )
+    print()
+    print("unicorn : $5.00 balance increases by $4")
+    print("horse: $0.50 balance decreases by $0.50")
+    print("zebra: $0.50 balance decreases by 0.50")
+    print("donkey: $0.00 balance decreases by $1")
+    print()
+    print("can you avoid the donkeys, get the unicorns and walk with cash in hand?")
     return ""
 
 
@@ -44,15 +55,62 @@ def num_check(question, low, high):
             else:
               print(error)
         except ValueError:
-          print(error)
+          print(error)     
+
+def statement_generator(statement, decoration):
+
+    sides = decoration * 3
+
+    statement = "{} } {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
 
 
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
 # Main Routine goes here...
 played_before = yes_no("have you played the game before? ")
-
+     
 if played_before == "no":
     instructions()
-print ("Program continues")
+print ("lets pay i mean play")
 # ask user how much they want to play with...
 how_much =num_check("how much would you like to pay with? ", 0, 10)
 print ("you will be spending ${}".format(how_much))
+
+balance = how_much
+
+rounds_played = 0
+
+play_again = input(" press enter to play...").lower()
+
+while play_again == "":
+
+  #increase # of rounds played
+  rounds_played += 1
+
+  #print round number
+  print()
+  print("*** Round #{} ***".format(rounds_played))
+
+  chosen_num = random.randint(1, 100)
+
+  #Adjust balance
+  #if the random # is between 1 and 5,
+  # user gets a unicorn (add $4 to balance)
+  if 1 <= chosen_num <=5:
+    chosen = "unicorn"
+    balance += 4
+    
+  if balance <1:
+    play_again = "xxx"
+    print("sorry you have run out of money")
+  else:
+    play_again = input("press enter to play again" "or 'xxx' to quit")
+  
+  
+print()
+print("final_balance", balance)
+     
